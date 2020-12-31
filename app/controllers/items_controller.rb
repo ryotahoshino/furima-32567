28af3364dbabb24799b7e3ec370ad1afb6b3f406
ryotahoshino <75008198+ryotahoshino@users.noreply.github.com>
2020-12-31 14:34:30 +0900

    モデルとコントローラーの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :detail, :price, :user, :state_id, :burden_id, :origin_id, :day_id, :category_id)
+  end
 end
---
+++ app/models/item.rb
@@ -0,0 +1,13 @@
+class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :detail, presence: true
+  validates :price, presence: true
+  validates :user, presence: true
+  validates :state_id, presence: true
+  validates :burden_id, presence: true
+  validates :origin_id, presence: true
+  validates :day_id, presence: true
+  validates :category_id, presence: true
+  belongs_to :user
+  has_one :record
+end
