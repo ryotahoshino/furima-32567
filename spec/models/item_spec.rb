b884e6cc56c352a51b792763d8464e5b3a7117e3
ryotahoshino <75008198+ryotahoshino@users.noreply.github.com>
2021-01-03 18:28:53 +0900

    テストコードの編集1

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
+describe Item do
+  before do
+    @item = FactoryBot.build(:item)
+  end
 
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザーの商品一覧表機能'
+    context '商品表示がうまくいく時' do
+      it "nameとdetailとpriceが存在すれば登録できる" do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品表示がうまくいかない時' do
+      it "nameが空だと登録できない" do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+
+      it "detailが空だと登録できない" do
+        @item.detail = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Detail can't be blank")
+      end
+
+      it "priceが空だと登録できない" do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+    end
+  end
 end
+
