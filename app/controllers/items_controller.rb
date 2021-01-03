387b654e9e920ecbb357b9fe327644374f552b90
ryotahoshino <75008198+ryotahoshino@users.noreply.github.com>
2021-01-03 17:16:49 +0900

    コントローラーの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,9 +10,24 @@ class ItemsController < ApplicationController
     Item.create(item_params)
   end
 
+  def edit
+  end
+
+  def update
+    if curent_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :detail, :price, :user, :state_id, :burden_id, :origin_id, :day_id, :category_id).merge(user_id: current_user.id)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
