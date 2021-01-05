9b30c215f86da9679d5500de974a087cbaecfd93
ryotahoshino <75008198+ryotahoshino@users.noreply.github.com>
2021-01-05 21:38:22 +0900

    テストコードの編集

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :item do
     name {Faker::Name.name}
     detail {"この商品の詳細"}
-    price {"810"}
-    user {current_user.id}
-    state_id {ItemId.all.sample}
-    burden_id {ItemId.all.sample}
-    origin_id {ItemId.all.sample}
-    day_id {ItemId.all.sample}
-    category_id {Itme.all.sample}
+    price {810}
+    state_id{"---"}
+    burden_id{"---"}
+    category_id{"---"}
+    day_id{"---"}
+    origin_id{"---"}
+    association :user
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
-describe Item do
-  before do
-    @item = FactoryBot.build(:item)
-  end
+
+RSpec.describe Item, type: :model do
+  describe '#create' do
+    before do
+      @item = FactoryBot.build(:item)
+    end
 
   describe 'ユーザーの商品一覧表機能'
     context '商品表示がうまくいく時' do
@@ -29,7 +31,36 @@ describe Item do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
+
+      it "state_idが空だと登録できない" do
+        @item.state_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("State can't be blank")
+      end
+
+      it "burden_idが空だと登録できない" do
+        @item.burden_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Burden can't be blank")
+      end
+
+      it "category_idが空だと登録できない" do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+
+      it "origin_idが空だと登録できない" do
+        @item.origin_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Origin can't be blank")
+      end
+
+      it "day_idが空だと登録できない" do
+        @item.day_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Day can't be blank")
+      end
     end
   end
 end
-
