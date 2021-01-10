1ed9412cad1aa1fc5a0b83b45e71c0f07a3d41c5
ryotahoshino <75008198+ryotahoshino@users.noreply.github.com>
2021-01-10 16:03:44 +0900

    コントローラーの編集

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -4,4 +4,25 @@ class AddressesController < ApplicationController
 
   def new
   end
+
+  def create
+    user = User.create(user_params)
+    Address.create(address_params(user))
+    Record.create(record_params(user))
+    redirect_to action: :index
+  end
+
+  private
+
+  def user_params
+    params.permit(:nickname)
+  end
+
+  def address_params
+    params.permit(:zip_code, :urban_id, :city, :address, :building, :telephone_number).merge(record_id: record.id)
+  end
+
+  def record_params
+    params.merge(item_id: item.id, user_id: user.id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_02_075435) do
+ActiveRecord::Schema.define(version: 2021_01_09_124622) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,19 @@ ActiveRecord::Schema.define(version: 2021_01_02_075435) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "zip_code", null: false
+    t.integer "urban_id", null: false
+    t.string "city", null: false
+    t.string "address", null: false
+    t.string "building"
+    t.string "telephone_number", null: false
+    t.bigint "record_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["record_id"], name: "index_addresses_on_record_id"
   end
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -63,6 +76,15 @@ ActiveRecord::Schema.define(version: 2021_01_02_075435) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "records", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "item_id"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_records_on_item_id"
+    t.index ["user_id"], name: "index_records_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -82,6 +104,9 @@ ActiveRecord::Schema.define(version: 2021_01_02_075435) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "addresses", "records"
   add_foreign_key "articles", "users"
   add_foreign_key "items", "users"
+  add_foreign_key "records", "items"
+  add_foreign_key "records", "users"
 end
