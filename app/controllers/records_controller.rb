4107ddb14e76c0276c6d03aa5eb7bbfce240bcd6
ryotahoshino <75008198+ryotahoshino@users.noreply.github.com>
2021-01-14 12:46:24 +0900

    コントローラーの編集

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,21 +1,22 @@
 class RecordsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create, :pay_item]
   def index
-    @record_address = RecordAddress.new
-    @item = Item.find(params[:item_id])
-  end
+    if @item.record.present?
+      redirect_to root_path
+    end
 
-  def new
+    @record_address = RecordAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @record_address = RecordAddress.new(address_params)
      if @record_address.valid?
       pay_item
       @record_address.save
       redirect_to root_path
      else
-      render 'index'
+      render :index
      end
   end
 
@@ -26,12 +27,15 @@ class RecordsController < ApplicationController
   end
 
   def pay_item
-    @item = Item.find(params[:item_id])
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item.price,
       card: address_params[:token],
       currency: 'jpy'
     )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
